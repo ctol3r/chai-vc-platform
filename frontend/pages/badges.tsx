ad161450dc805eb2fefb76be2365aa9b7097d57e
ctol3r <tolerc@pamf.org>
2025-07-12 21:02:38 -0700

    Fix placeholder test

---
+++ frontend/pages/badges.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BadgesPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Issuer Badges</h1>
+      <p>High-performing issuers can earn badges for completing credentialing milestones.</p>
+    </div>
+  );
+};
+
+export default BadgesPage;
