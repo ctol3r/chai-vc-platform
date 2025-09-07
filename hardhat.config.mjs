48ed2001272acecb83281057a5103ffc5fa3d77e
Chris Toler <christ0phr@outlook.com>
2025-09-07 14:12:59 -0700

    build: Hardhat 3 + ethers 6; ESM config; test script = hardhat

--- hardhat.config.mjs
+++ hardhat.config.mjs
@@ -1,15 +1,3 @@
-import '@nomicfoundation/hardhat-toolbox';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-export default {
-  solidity: {
-    compilers: [
-      {
-        version: '0.8.20',
-        // Use solc-js from node_modules to avoid downloads
-        path: require.resolve('solc'),
-      },
-    ],
-  },
-};
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+export default { solidity: "0.8.20" };
